f69c9573d3e3cce5283dc94ab52358226c4319c6
naolaboma <naolaboma@gmail.com>
2025-11-07 23:18:03 +0300

    implemented the usecases

--- internal/usecase/invoice_usecase.go
+++ internal/usecase/invoice_usecase.go
@@ -1,1 +1,55 @@
 package usecase
+
+import (
+	"Invoice-Payment-System/internal/domain"
+	"errors"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type InvoiceUseCase struct {
+	InvoiceRepo domain.InvoiceRepository
+}
+
+func NewInvoiceUseCase(repo domain.InvoiceRepository) domain.InvoiceUsecase {
+	return &InvoiceUseCase{
+		InvoiceRepo: repo,
+	}
+}
+
+func (uc *InvoiceUseCase) CreateInvoice(invoice *domain.Invoice) error {
+	if invoice.Amount <= 0 {
+		return errors.New("invoice amount must be positive")
+	}
+	invoice.Status = "PENDING"
+	invoice.CreatedAt = time.Now()
+	invoice.UpdatedAt = time.Now()
+	invoice.ID = primitive.NewObjectID()
+	return uc.InvoiceRepo.Create(invoice)
+}
+
+func (uc *InvoiceUseCase) GetInvoice(id string) (*domain.Invoice, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, errors.New("invalid invoice ID format")
+	}
+	return uc.InvoiceRepo.FindById(objectID)
+}
+
+func (uc *InvoiceUseCase) UpdateInvoiceStatus(id string, status string) error {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return errors.New("invalid invoice ID format")
+	}
+	return uc.InvoiceRepo.UpdateStatus(objectID, status)
+}
+
+func (uc *InvoiceUseCase) GetInvoicesBySender(email string) ([]*domain.Invoice, error) {
+	invoices, err := uc.InvoiceRepo.FindBySenderEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	return invoices, nil
+
+}
--- internal/usecase/payment_usecase.go
+++ internal/usecase/payment_usecase.go
@@ -1,1 +1,53 @@
 package usecase
+
+import (
+	"Invoice-Payment-System/internal/domain"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type PaymentUseCase struct{
+	InvoiceRepo domain.InvoiceRepository
+	PaymentRepo domain.PaymentRepository
+}
+
+func NewPaymentUseCase(invoicerepo domain.InvoiceRepository, paymentrepo domain.PaymentRepository) domain.PaymentUsecase{
+	return &PaymentUseCase{
+		InvoiceRepo: invoicerepo,
+		PaymentRepo: paymentrepo,
+	}
+}
+
+func (uc *PaymentUseCase) HandleCallBack(payment *domain.Payment) error{
+	payment.CreatedAt = time.Now()
+	payment.UpdatedAt = time.Now()
+	
+	err := uc.PaymentRepo.LogPayment(payment)
+	if err != nil{
+		return err
+	}
+	
+	switch payment.Status {
+	case "SUCCESS":
+		return uc.InvoiceRepo.UpdateStatus(payment.InvoiceID, "PAID")
+	case "FAILED":
+		return uc.InvoiceRepo.UpdateStatus(payment.InvoiceID, "FAILED")
+	default:
+		// do nothing for pending or unknown statuses
+		return nil
+	}
+}
+
+func (uc *PaymentUseCase) GetPaymentByReference(ref string) (*domain.Payment, error){
+	return uc.PaymentRepo.FindByReference(ref)
+}
+
+func (uc *PaymentUseCase) GetPaymentsByInvoice(invoiceID string) ([]*domain.Payment, error){
+	objectID, err := primitive.ObjectIDFromHex(invoiceID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid invoice ID: %v", err)
+	}
+	return uc.PaymentRepo.FindByInvoiceID(objectID)
+}
