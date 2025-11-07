3f0598c2e3ed966dc06c4681fdab86aa47d141f4
naolaboma <naolaboma@gmail.com>
2025-11-07 10:59:25 +0300

    updated the invoice repository

--- internal/domain/invoice.go
+++ internal/domain/invoice.go
@@ -1,32 +1,33 @@
 package domain
 
 import (
-	"context"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Invoice struct {
-	ID           string    `bson:"_id,omitempty" json:"id"`
-	SenderEmail  string    `bson:"senderEmail" json:"senderEmail"`
-	PayerEmail   string    `bson:"payerEmail" json:"payerEmail"`
-	Amount       float64   `bson:"amount" json:"amount"`
-	Currency     string    `bson:"currency" json:"currency"`
-	Description  string    `bson:"description" json:"description"`
-	Status       string    `bson:"status" json:"status"`
-	SantimPayRef *string   `bson:"santimPayRef,omitempty" json:"santimPayRef,omitempty"`
-	CreatedAt    time.Time `bson:"createdAt" json:"createdAt"`
-	UpdatedAt    time.Time `bson:"updatedAt" json:"updatedAt"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	SenderEmail  string             `bson:"senderEmail" json:"senderEmail"`
+	PayerEmail   string             `bson:"payerEmail" json:"payerEmail"`
+	Amount       float64            `bson:"amount" json:"amount"`
+	Currency     string             `bson:"currency" json:"currency"`
+	Description  string             `bson:"description" json:"description"`
+	Status       string             `bson:"status" json:"status"`
+	SantimPayRef *string            `bson:"santimPayRef,omitempty" json:"santimPayRef,omitempty"`
+	CreatedAt    time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt    time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
 
 // interfaces
 type InvoiceRepository interface {
-	Create(ctx context.Context, invoice *Invoice) error
-	FindById(ctx context.Context, id string) (*Invoice, error)
-	UpdateStatus(ctx context.Context, id string, status string) error
+	Create(invoice *Invoice) error
+	FindById(id primitive.ObjectID) (*Invoice, error)
+	UpdateStatus(id primitive.ObjectID, status string) error
 }
 
 type InvoiceUsecase interface {
-	CreateInvoice(ctx context.Context, invoice *Invoice) error
-	GetInvoice(ctx context.Context, id string) (*Invoice, error)
-	UpdateInvoice(ctx context.Context, id string, status string) error
+	CreateInvoice(invoice *Invoice) error
+	GetInvoice(id string) (*Invoice, error)
+	UpdateInvoice(id string, status string) error
 }
--- internal/repository/invoice_repository.go
+++ internal/repository/invoice_repository.go
@@ -1,1 +1,93 @@
 package repository
+
+import (
+	"Invoice-Payment-System/internal/domain"
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"Invoice-Payment-System/internal/infrastructure/database"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type InvoiceRepo struct {
+	db *database.MongoDB
+	collection *mongo.Collection
+}
+
+func NewInvoiceRepository(db *database.MongoDB) domain.InvoiceRepository{
+		collection := db.GetCollection("invoices")
+		return &InvoiceRepo{db: db, collection: collection}
+}
+
+func (ir *InvoiceRepo) Create(invoice *domain.Invoice) error{
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	invoice.CreatedAt = time.Now()
+	invoice.UpdatedAt = time.Now()
+	
+	_, err := ir.collection.InsertOne(ctx, invoice)
+	if err != nil{
+		if mongo.IsDuplicateKeyError(err) {
+			return fmt.Errorf("invoice with id already exists: %w", err)
+		}
+		if errors.Is(err, context.DeadlineExceeded){
+			return fmt.Errorf("database context deadline exceeded: %w", err)
+		}
+		
+		return fmt.Errorf("failed to create invoice: %w", err)
+	}
+	
+	return nil
+}
+
+func (ir *InvoiceRepo) FindById(id primitive.ObjectID) (*domain.Invoice, error){
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	var invoice domain.Invoice
+	filter := bson.M{"_id": id}
+	err := ir.collection.FindOne(ctx, filter).Decode(&invoice)
+	
+	if err != nil{
+		if err == mongo.ErrNoDocuments{
+			return nil, errors.New("invoice not found")
+		}
+		return nil, fmt.Errorf("database error in findById: %w", err)
+	}
+	return &invoice, nil
+}
+
+func(ir *InvoiceRepo) UpdateStatus(id primitive.ObjectID, status string) error{
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	update := bson.M{
+		"$set": bson.M{
+			"status": status,
+			"updatedAt": time.Now(),
+		},
+	}
+	
+	opts := options.Update().SetUpsert(false)
+	res, err := ir.collection.UpdateByID(ctx, id, update, opts)
+	
+	if err != nil{
+		if errors.Is(err, context.DeadlineExceeded){
+			return fmt.Errorf("database cotext deadline exceeded: %w", err)
+		}
+		return fmt.Errorf("failed to update invoice status: %w", err)
+	}
+	
+	if res.MatchedCount == 0{
+		return fmt.Errorf("invouce not found")
+	}
+	
+	return nil
+}
