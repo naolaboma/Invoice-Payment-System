0181d157e0eff8312a3cc0c92c50679c085e4cf0
naolaboma <naolaboma@gmail.com>
2025-11-08 20:10:32 +0300

    completed the invoice controller

--- internal/delivery/controllers/invoice_controller.go
+++ internal/delivery/controllers/invoice_controller.go
@@ -1,1 +1,53 @@
 package controller
+import(
+	"Invoice-Payment-System/internal/domain"
+	"net/http"
+	
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type InvoiceHandler struct{
+	InvoiceUsecase domain.InvoiceUsecase
+	Validate *validator.Validate
+}
+
+func NewInvoiceController(invoiceUsecase domain.InvoiceUsecase) *InvoiceHandler{
+	return &InvoiceHandler{
+		InvoiceUsecase: invoiceUsecase,
+		Validate: validator.New(),
+	}
+}
+
+func (h *InvoiceHandler) CreateInvoice(c *gin.Context) {
+	var invoice domain.Invoice
+	if err := c.ShouldBindBodyWithJSON(&invoice); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	
+	if err := h.Validate.Struct(invoice); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"validation_error": err.Error()})
+		return
+	}
+	
+	err := h.InvoiceUsecase.CreateInvoice(&invoice)
+	if err !=nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	
+	c.JSON(http.StatusCreated, invoice)
+}
+
+func (h *InvoiceHandler) GetInvoicesBySender(c *gin.Context) {
+	email := c.Query("email")
+	
+	invoices, err := h.InvoiceUsecase.GetInvoicesBySender(email)
+	if err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	
+	c.JSON(http.StatusOK, invoices)
+}
